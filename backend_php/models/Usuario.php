7535bdd6e0b5c79240c252c9c446282f6f1687eb
CarraroGabriel <gabrielcarraro.gb@gmail.com>
2026-05-31 18:44:49 -0300

    Adição models e Atualização da model Usuário

--- backend_php/models/Usuario.php
+++ backend_php/models/Usuario.php
@@ -3,24 +3,36 @@
 class Usuario
 {
     public ?int $id_usuario;
-    public string $nm_login;
+    public string $nu_cpf;
+    public string $nm_usuario;
+    public string $ds_email;
+    public string $dt_nascimento;
     public string $ds_senha;
 
     public function __construct(
         ?int $id_usuario,
-        string $nm_login,
+        string $nu_cpf,
+        string $nm_usuario,
+        string $ds_email,
+        string $dt_nascimento,
         string $ds_senha
     ) {
-        $this->id_usuario = $id_usuario;
-        $this->nm_login = $nm_login;
-        $this->ds_senha = $ds_senha;
+        $this->id_usuario    = $id_usuario;
+        $this->nu_cpf        = $nu_cpf;
+        $this->nm_usuario    = $nm_usuario;
+        $this->ds_email      = $ds_email;
+        $this->dt_nascimento = $dt_nascimento;
+        $this->ds_senha      = $ds_senha;
     }
 
     public static function fromArray(array $dados): Usuario
     {
         return new Usuario(
             isset($dados["id_usuario"]) ? (int) $dados["id_usuario"] : null,
-            trim($dados["nm_login"] ?? ""),
+            trim($dados["nu_cpf"] ?? ""),
+            trim($dados["nm_usuario"] ?? ""),
+            trim($dados["ds_email"] ?? ""),
+            trim($dados["dt_nascimento"] ?? ""),
             trim($dados["ds_senha"] ?? "")
         );
     }
